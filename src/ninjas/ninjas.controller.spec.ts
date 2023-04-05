f398f67fa3ed8a76edd95e067dc07d0e43932696
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-04-05 17:26:24 +0700

    init

--- src/ninjas/ninjas.controller.spec.ts
+++ src/ninjas/ninjas.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NinjasController } from './ninjas.controller';
+import { NinjasService } from './ninjas.service';
 
 describe('NinjasController', () => {
   let controller: NinjasController;
@@ -7,6 +8,7 @@ describe('NinjasController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NinjasController],
+      providers: [NinjasService],
     }).compile();
 
     controller = module.get<NinjasController>(NinjasController);
