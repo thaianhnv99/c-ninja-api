1cd3594eea5ff796961c8a2b1ed70a55e06bdbbd
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-04-07 17:31:15 +0700

    add access token

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,17 @@ import { BeltGuard } from './belt/belt.guard';
 import { RequestService } from './request.service';
 import { UsersService } from './users/users.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [NinjasModule, UsersModule, AuthModule],
+  imports: [
+    NinjasModule,
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    BookmarkModule,
+  ],
   controllers: [AppController, NinjasController],
   providers: [
     AppService,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
   private account;
 
   setAccount(account: any) {
     this.account = account;
   }
 
-  login() {}
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // Save the new user in the db
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-  signup() {}
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
+  }
+
+  async login(dto: AuthDto) {
+    // find the ser by email
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exits throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: process.env.JWT_SECRET,
+    });
+
+    return { access_token: token };
+  }
 }
